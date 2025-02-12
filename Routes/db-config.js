29255f7ab545a818c704ed0dd852e60fbc633211
Amu Kabir <ameyakabir@gmail.com>
2025-02-12 23:28:38 +0530

    DB connected to freedb.tech

--- Routes/db-config.js
+++ Routes/db-config.js
@@ -1,10 +1,17 @@
-const sql = require("mysql");
+const sql = require("mysql2");
 const dotenv = require("dotenv").config();
 const db = sql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  authPlugins: {
+    mysql_clear_password: () => () =>
+      Buffer.from(process.env.DATABASE_PASSWORD),
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = db;
