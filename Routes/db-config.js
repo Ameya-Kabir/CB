dab4b7bb7d781949cc9cf1abba929bd3f0f57db0
Amu Kabir <ameyakabir@gmail.com>
2025-02-13 01:07:37 +0530

    changes in env

--- Routes/db-config.js
+++ Routes/db-config.js
@@ -1,10 +1,11 @@
 const sql = require("mysql2");
 const dotenv = require("dotenv").config();
 const db = sql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  host: "sql.freedb.tech",
+  user: "freedb_bakery",
+  password: "cu*%P%4dDHXh8ct",
+  database: "freedb_crown_bakers",
+  port: 3306,
   authPlugins: {
     mysql_clear_password: () => () =>
       Buffer.from(process.env.DATABASE_PASSWORD),
